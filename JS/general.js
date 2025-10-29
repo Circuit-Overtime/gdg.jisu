e5412adc1615e6e73e8e2ff886719068063cc1e8
Ayushman Bhattacharya <74301576+Circuit-Overtime@users.noreply.github.com>
2025-10-29 13:10:35 +0530

    updated code to redirect correctly to / routing

---
+++ JS/general.js
@@ -0,0 +1,7 @@
+document.getElementById("gnitTile").addEventListener("click", function() {
+    redirectTo("gnit/");
+});
+
+document.getElementById("jisuTile").addEventListener("click", function() {
+    redirectTo("jisu/");
+});
